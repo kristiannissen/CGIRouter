7f9d28ad898c4a32ab58c4a08610660b6dd9621c
m4ddav3 <m4ddav3@users.noreply.github.com>
2015-03-05 15:52:24 +0000

    Enable building of routes table
    Previous design was taking a call, building a route for it, then
    executing the handler. That defeats the purpose of routing.
    
    This change allows you to build a routing table by calling:
    
        add_route($method, $route, $handler)
    
    `$method` is an HTTP method (GET|PUT|POST|etc..)
    `$route` is the route (`/`, `/foo/:bar`, ..)
    `$handler` is the sub which runs when this route is invoked
    
    Other changes:
    
    1. Always return from a method. If nothing particular is returned,
    return $self so that methods can be chained
    2. `db => $config->{db} //= {}` isn't very clear/might have
    side effects. Moved the `//=` parts into their own paragraph
    3. Hint to Perl and the programmer that the value returned by
    `build_pattern` is a regex, by returning `qr/$pattern/;`
    
    `example.pl` added to show usage

--- CGI/Router.pm
+++ CGI/Router.pm
@@ -4,177 +4,201 @@ use strict;
 use warnings;
 
 use parent 'CGI';
-use Cwd 'abs_path';
-use Carp;
+
+use Cwd  qw(abs_path);
+use Carp qw(croak);
 
 use Data::Dumper;
 
 sub setup {
-  my ( $self, $config ) = @_;
+    my ( $self, $config ) = @_;
 
-  $self->{config} = {
-    db => $config->{db} //= {},
-    log => $config->{log} //= {},
-    layout => $config->{layout} //= {},
-    hooks => $config->{hooks} //= {}
-  };
+    $config->{db}     //= {};
+    $config->{log}    //= {};
+    $config->{layout} //= {};
+    $config->{hooks}  //= {};
+
+    $self->{config} = {
+        db     => $config->{db},
+        log    => $config->{log},
+        layout => $config->{layout},
+        hooks  => $config->{hooks},
+    };
+
+    return $self;
 }
 
-sub connect {
-  my ( $self, $req, $subr ) = @_;
-  
-  $self->{routes} //= {};
-  $self->{env} //= \%ENV;
+sub add_route {
+    my ($self, $method, $route, $handler) = @_;
 
-  if ( ! exists $self->{routes}->{$req} ) {
-    $self->{routes}->{$req} = {
-      handler => $subr,
-      pattern => $self->build_pattern( $req ),
-      method => $req =~ /^(GET|PUT|POST|DELETE)/
-    };
-  } else {
-    Carp::croak( "Similar request already exists $req!" );
-  }
-  
-  # Get current request destination
-  # TODO: Add that stupid IIS HTTP header
-  $self->{destination} = $self->{env}->{REQUEST_URI};
-  $self->{method} = $self->{env}->{REQUEST_METHOD};
+    $self->{routes} //= {};
+
+    if ( ! exists $self->{routes}->{$method}->{$route} ) {
+        $self->{routes}->{$method}->{$route} = {
+            handler => $handler,
+            pattern => $self->build_pattern( $route ),
+            method  => $method,
+        };
+    } else {
+        croak( "Similar request already exists $method $route!" );
+    }
+
+    return $self;
 }
 
 sub render_markup {
-  my ( $self, $template_file, $template_vars ) = @_;
-  my $conf = $self->{config}->{layout};
+    my ( $self, $template_file, $template_vars ) = @_;
+    my $conf = $self->{config}->{layout};
 
-  my $template = "$conf->{templatepath}/$template_file";
-  my %template_vars = %{$template_vars};
-  
-  my $output;
-  
-  if ( open( my $fh, "<:encoding(UTF-8)", $template) ) {
-    while ( my $row = <$fh> ) {
-      chomp $row;
-      $output .= $row;
+    my $template      = "$conf->{templatepath}/$template_file";
+    my %template_vars = %{$template_vars};
+
+    my $output;
+
+    if ( open( my $fh, "<:encoding(UTF-8)", $template ) ) {
+        while ( my $row = <$fh> ) {
+            $row =~ s{[\f\n\r]*$}{};
+            $output .= $row;
+        }
+        close($fh);
     }
-    close( $fh );
-  } else {
-    Carp::croak( "Could not open $template" );
-  }
-  
-  foreach my $key ( keys \%template_vars ) {
-    my $val = $template_vars{$key};
-    my $pattern = $key;
-    
-    $output =~ s/{$key}/$val/g;
-  }
-  
-  if ( exists $conf->{master} ) {
-    my $master = "$conf->{templatepath}/$conf->{master}";
-    my $master_output;
-    if ( open( my $fh, "<:encoding(UTF-8)", $master ) ) {
-      while ( my $row = <$fh> ) {
-        chomp $row;
-        $master_output .= $row;
-      }
-    } else {
-      Carp::croak( "Could not open $master" );
+    else {
+        croak("Could not open $template");
     }
-    
-    $master_output =~ s/{yield}/$output/g;
-    $output = $master_output;
-  }
 
-  $self->logger( 'Rendering output' );
+    foreach my $key ( keys \%template_vars ) {
+        my $val     = $template_vars{$key};
+        my $pattern = $key;
 
-  $self->set_header ( $template_file =~ /\.([a-z]{1,})/ );
-  print $output;
+        $output =~ s/{$key}/$val/g;
+    }
+
+    if ( exists $conf->{master} ) {
+        my $master = "$conf->{templatepath}/$conf->{master}";
+        my $master_output;
+        if ( open( my $fh, "<:encoding(UTF-8)", $master ) ) {
+            while ( my $row = <$fh> ) {
+                chomp $row;
+                $master_output .= $row;
+            }
+        }
+        else {
+            croak("Could not open $master");
+        }
+
+        $master_output =~ s/{yield}/$output/g;
+        $output = $master_output;
+    }
+
+    $self->logger('Rendering output');
+
+    $self->set_header( $template_file =~ /\.([a-z]{1,})/ );
+    print $output;
+
+    return $self;
 }
 
 sub render_txt {
-  my ( $self, $txt ) = @_;
-  
-  print $txt;
+    my ( $self, $txt ) = @_;
+
+    print $txt;
+
+    return $self;
 }
 
 sub set_header {
-  my ( $self, $content_type ) = @_;
+    my ( $self, $content_type ) = @_;
 
-  if ( lc $content_type eq 'html' ) {
-    print $self->header( -type => 'text/html', -charset => 'utf-8' );
-  }
+    if ( lc $content_type eq 'html' ) {
+        print $self->header( -type => 'text/html', -charset => 'utf-8' );
+    }
+
+    return $self;
 }
 
 sub mapper {
-  my $self = shift;
-  
-  my $router;
-  my @params;
-  
-  foreach my $key ( keys %{ $self->{routes} } ) {
-    my $route = $self->{routes}->{$key};
-    
-    if ( $self->{method} eq $route->{method} &&
-      $self->{destination} =~ $route->{pattern} ) {
-      
-      @params = $self->{destination} =~ $route->{pattern};
+    my ($self) = @_;
 
-      $router = $route;
+    my $router;
+    my @params;
+
+    my $method = $ENV{'REQUEST_METHOD'};
+    my $uri    = $ENV{'REQUEST_URI'};
+
+    foreach my $key ( keys %{ $self->{routes}->{$method} } ) {
+        my $route = $self->{routes}->{$method}->{$key};
+
+        if ($uri =~ $route->{pattern}) {
+            # Found the matching route
+            @params = $uri =~ $route->{pattern};
+            $router = $route;
+
+            # Stop looking for more routes
+            last;
+        }
     }
-  }
 
-  # Run hooks
-  $self->run_hooks;
+    # Run hooks
+    $self->run_hooks();
 
-  return $router->{handler}->( @params );
+    # Handle the route
+    return $router->{handler}->( @params );
 }
 
 sub logger {
-  my ( $self, $msg ) = @_;
-  
-  if ( exists $self->{log} ) {
-    my $file_path = $self->{config}->{log}->{path};
-    my $level = $self->{config}->{log}->{level};
+    my ( $self, $msg ) = @_;
 
-    open my $fh, ">>", "$file_path/$level.log";
-    print $fh sprintf("[T:%s] [%s] - %s \n", time, $level, $msg);
-    close $fh;
-  }
+    if ( exists $self->{log} ) {
+        my $file_path = $self->{config}->{log}->{path};
+        my $level = $self->{config}->{log}->{level};
+
+        open my $fh, ">>", "$file_path/$level.log";
+        print $fh sprintf("[T:%s] [%s] - %s \n", time, $level, $msg);
+        close $fh;
+    }
+
+    return $self;
 }
 
 sub run_hooks {
-  my $self = shift;
-  my $hooks = $self->{config}->{hooks};
-  
-  $self->logger( 'Running hooks before_each' );
-  # Run each subroutine
-  $hooks->{before_each}->( $self ) if ref $hooks->{before_each} eq 'CODE';
+    my ($self) = @_;
+    my $hooks = $self->{config}->{hooks};
+
+    $self->logger( 'Running hooks before_each' );
+    # Run each subroutine
+    if (ref $hooks->{before_each} eq 'CODE') {
+        $hooks->{before_each}->( $self );
+    }
+
+    return $self;
 }
 
 sub build_pattern {
-  my ( $self, $pattern ) = @_;
-  # Remov method from pattern, substitute it with nothing
-  $pattern =~ s/(GET|POST|PUT|DELETE)\s?//i;
-  # do block returns complex regex
-  $pattern = do {
-    # Replace something like /word/:token with /word/(^:([a-z]+))
-    $pattern =~ s!
-      (\:([a-z]+))
-    !
-      if ( $2 ) {
-        "([^/]+)"
-      }
-    !gex;
-    
-    "^$pattern\$";
-  };
+    my ( $self, $pattern ) = @_;
 
-  return $pattern;
+    # do block returns complex regex
+    $pattern = do {
+        # Replace something like /word/:token with /word/(^:([a-z]+))
+        $pattern =~ s!
+            (\:([a-z]+))
+        !
+            if ( $2 ) {
+                "([^/]+)"
+            }
+        !gex;
+
+        "^$pattern\$";
+    };
+
+    # Hint to Perl that this is a regular expression pattern
+    # Also makes it more obvious what this is for
+    return qr/$pattern/;
 }
 
 sub run {
-  my $self = shift;
-  
-  return $self->mapper();
+    my ($self) = @_;
+
+    return $self->mapper();
 }
 
 1;
